069fc79db388c148b56d88a2c1cc7fe3aef603fa
isilay-subasi <isil.subasi19@gmail.com>
2022-01-06 20:16:43 +0300

    State Hoisting

--- app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
@@ -3,25 +3,20 @@ package com.example.statemanagementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.statemanagementcompose.ui.theme.StateManagementComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,49 +28,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(){
+    //State Hoisting
+    var myString = remember { mutableStateOf("")}
 
-    Surface(color = Color.LightGray) {
-        Column(modifier = Modifier.fillMaxSize(),
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-
         ) {
-
-            var myString=remember{mutableStateOf("Android Compose")}
-            TextField(value = myString.value,
-                onValueChange = {
-                    myString.value=it
-                    println(myString.value)
-                })
-
-
-            var myTextString=remember{ mutableStateOf("Hello!")}
-            Text(text = myTextString.value,
-            fontSize = 25.sp)
-            Spacer(modifier = Modifier.padding(7.dp))
-            Button(onClick = {
-                myTextString.value="Android"
-            }) {
-                Text(text = "Button")
-                Text(text = "Test")
+            SpecialText("Test 1")
+            Spacer(modifier = Modifier.size(5.dp))
+            SpecialText(string = "Test 2")
+            Spacer(modifier = Modifier.size(5.dp))
+            SpecialTextField(myString.value){
+                myString.value=it
             }
-            Spacer(modifier = Modifier.padding(7.dp))
-            Image(bitmap = ImageBitmap.imageResource(id = R.drawable.flower),
-            contentDescription = "flower",
-            )
-            Spacer(modifier = Modifier.padding(7.dp))
-            Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
-                contentDescription =null )
-            Spacer(modifier = Modifier.padding(7.dp))
-            Image(painter = ColorPainter(Color.Green), contentDescription =null ,
-            modifier = Modifier.size(20.dp))
-            
         }
     }
 
 
+
+
 }
+
+
+@Composable
+fun SpecialText(string : String){
+    Text(text = string,
+        fontSize = 16.sp,
+        fontFamily = FontFamily.Monospace,
+        fontStyle = FontStyle.Italic
+    )
+}
+
+@Composable
+fun SpecialTextField(string: String , function : (String) -> Unit){
+    TextField(value =string , onValueChange = function)
+
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
