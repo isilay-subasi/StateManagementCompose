dc8cd2d9a74a7ee476f2e270d34b7db016cdec63
isilay-subasi <isil.subasi19@gmail.com>
2022-01-06 12:40:35 +0300

    Button

--- app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.statemanagementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.statemanagementcompose.ui.theme.StateManagementComposeTheme
 
@@ -37,6 +37,13 @@ fun MainScreen() {
         ) {
             Text(text = "Hello!",
             fontSize = 25.sp)
+            Spacer(modifier = Modifier.padding(7.dp))
+            Button(onClick = {
+                println("clicked")
+            }) {
+                Text(text = "Button")
+                Text(text = "Test")
+            }
         }
     }
 
