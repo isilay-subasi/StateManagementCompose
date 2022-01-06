b3ad723e6b21cda82b058a8029200f8d907af0d9
isilay-subasi <isil.subasi19@gmail.com>
2022-01-06 12:58:24 +0300

    Image

--- app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.statemanagementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -12,6 +13,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,6 +50,17 @@ fun MainScreen() {
                 Text(text = "Button")
                 Text(text = "Test")
             }
+            Spacer(modifier = Modifier.padding(7.dp))
+            Image(bitmap = ImageBitmap.imageResource(id = R.drawable.flower),
+            contentDescription = "flower",
+            )
+            Spacer(modifier = Modifier.padding(7.dp))
+            Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
+                contentDescription =null )
+            Spacer(modifier = Modifier.padding(7.dp))
+            Image(painter = ColorPainter(Color.Green), contentDescription =null ,
+            modifier = Modifier.size(20.dp))
+            
         }
     }
 
